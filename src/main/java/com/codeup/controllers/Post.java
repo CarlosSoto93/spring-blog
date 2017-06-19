cb5cf007db41d87cbd4d2f063cad6dc40f641814
Carlos <c.soto93@hotmail.com>
2017-06-19 16:24:26 -0500

    Dice guessing game exercise completed

---
+++ src/main/java/com/codeup/controllers/Post.java
@@ -0,0 +1,7 @@
+package com.codeup.controllers;
+
+/**
+ * Created by Carlos on 6/19/17.
+ */
+public class Post {
+}
--- src/main/java/com/codeup/controllers/ViewController.java
+++ src/main/java/com/codeup/controllers/ViewController.java
@@ -1,15 +1,46 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by Carlos on 6/19/17.
  */
 @Controller
 public class ViewController {
+
+
     @GetMapping("/roll-dice")
-    public int getNumber() {
-        return 
+    public String rollDice() {
+        return "roll-dice";
     }
+
+    @GetMapping("/roll-dice/{number}")
+    public String valuePicked(@PathVariable int number , Model model) {
+        model.addAttribute("number", number);
+        int random = (int) (Math.random()*6+1);
+        model.addAttribute("dice", random);
+        if(random == number) {
+            model.addAttribute("message", "Good guess, you win");
+        } else{
+            model.addAttribute("message", "Sorry, try again");
+        }
+        return "roll-dice";
+    }
+
+
+
+
+    @GetMapping("/portfolio")
+    public String portfolio() {
+        return "portfolio";
+    }
+
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
+    }
+
 }
